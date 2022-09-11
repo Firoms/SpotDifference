8d845055bf27e5602d26a61e801754bc586c07e9
firoms <edisonmessi@naver.com>
2022-09-11 14:19:17 +0900

    modeling

---
+++ python/codes/win.py
@@ -0,0 +1,24 @@
+
+
+from tkinter import *
+
+
+class Win:
+    def __init__(self):
+        self.win = Tk()
+        self.win.title("틀린 그림 찾기")
+        # self.win.iconbitmap() > 아이콘
+        self.win.geometry("1300x700")
+        self.win.mainloop()
+    
+    def main_menu(self):
+        pass
+    
+    def game(self):
+        pass
+    
+    def ending(self):
+        pass
+    
+
+game_start = Win()
